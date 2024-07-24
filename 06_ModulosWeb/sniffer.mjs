c69c01cfed55abd6e80cd65e3f3be4fbe7b12444
YonyChaparro <ychaparro@unal.edu.co>
2024-07-24 12:32:28 -0500

    sniffer v1.0.0

---
+++ 06_ModulosWeb/sniffer.mjs
@@ -0,0 +1,43 @@
+import { createServer } from "http";
+import { get } from "https";
+
+const hostname = "localhost",
+  port = 3000,
+  options = {
+    host: "yonychaparro.github.io",
+    port: 443,
+    path: "/YonyChaparroCV/",
+  };
+
+let htmlCode = "";
+
+const httpClient = (res) => {
+  console.log(
+    `El sitio ${options.host} ha respondido. Código: ${res.statusCode}. Mensaje: ${res.statusMessage}.`
+  );
+
+  res.on("data", (data) => {
+    htmlCode += data;
+    console.log(data, data.toString());
+  });
+};
+
+const httpError = (err) => {
+  console.error(
+    `El sitio ${options.host} no ha respondido. Código: ${err.code}. Mensaje: ${err.message}.`
+  );
+};
+
+const webServer = (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.end(htmlCode);
+};
+
+//instancia cliente HTTP o HTTPs
+get(options, httpClient).on("error", httpError);
+
+//instancia servidor local HTTP
+createServer(webServer).listen(port, hostname, () => {
+  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+});
